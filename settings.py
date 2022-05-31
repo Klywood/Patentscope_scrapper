5f4249e58ae1cc136d41aa3fe12072f42043efa7
Andrew <gish.gish@inbox.ru>
2022-05-31 11:40:37 +0300

    update selectors, add logger params

--- settings.py
+++ settings.py
@@ -1,4 +1,11 @@
 
+#  LOGGER PARAMS
+LOG_FOLDER = 'logs'
+LOG_NAME = 'Patents'
+LOG_LEVEL = 'DEBUG'
+FH_level = 'DEBUG'
+SH_level = 'DEBUG'
+
 #  SELECTORS
 #  search_button selector (XPATH)
 search_button_selector = '//*[@id="simpleSearchForm:fpSearch:buttons"]/button'
@@ -8,8 +15,8 @@ per_page_selector = '//*[@id="resultListCommandsForm:perPage:input"]'
 #  how to sort results selector (XPATH)
 sort_by_selector = '//*[@id="resultListCommandsForm:sort:input"]'
 
-#  elements with patents info (CLASS_NAME)
-patents_selector = 'ps-patent-result'
+#  elements with patents info (CSS_SELECTOR)
+patents_selector = '.ps-patent-result'
 #  title selector
 title_selector = '.ps-patent-result--first-row .trans-section'
 #  abstracts selector
@@ -20,18 +27,9 @@ links_selector = '.ps-patent-result--first-row a'
 dates_selector = '.ps-patent-result--title--ctr-pubdate span:nth-child(3)'
 #  patent's class selector (CSS_SELECTOR)
 class_selector = '.ps-patent-result--second-row a'
-#  patent's applicant selector (CLASS_NAME)
-applicants_selector = '.ps-patent-result--second-row a'
+#  patent's applicant selector (CSS_SELECTOR)
+applicants_selector = '.ps-patent-result--applicant'
 #  patent's inventors selector (CLASS_NAME)
 inventors_selector = '.ps-patent-result--inventor'
 #  next_button selector (CSS_SELECTOR)
 next_button_selector = '.js-paginator-next'
-
-
-title = '.ps-patent-result--first-row .trans-section'
-link = '.ps-patent-result--first-row a'
-patent_class = '.ps-patent-result--second-row a'
-applicant = '.ps-patent-result--applicant'
-inventor = '.ps-patent-result--inventor'
-date = '.ps-patent-result--title--ctr-pubdate span:nth-child(3)'
-abstracts = '.ps-patent-result--second-row .trans-section'
